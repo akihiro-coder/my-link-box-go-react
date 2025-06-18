131bb98567090fe7700f6375d26bcd20a6a6dc35
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-06-18 18:14:47 +0900

    [#1]checked frontend get data from backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [apiStatus, setApiStatus ] = useState('Loading...');
+
+  useEffect(() => {
+    // Call the healthcheck API for the backend
+    fetch('http://localhost:8080/api/health')
+      .then(response => response.json())
+      .then(data => {
+        setApiStatus(`API Status: ${data.status} - ${data.message}`);
+      })
+      .catch(error => {
+        setApiStatus(`Error connecting to API: ${error.message}`);
+      });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My-Link-Box!</h1>
+        <p>Bookmarks management App</p>
+        <div>{apiStatus}</div>
       </header>
     </div>
   );
